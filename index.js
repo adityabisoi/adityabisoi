b1a82fb8d20d444979d04ab83b05d782566e681b
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-10-03 22:15:42 +0530

    Add configuration

---
+++ index.js
@@ -0,0 +1,16 @@
+const ejs = require('ejs')
+const fs = require('fs')
+const axios = require('axios')
+
+axios.get('https://api.github.com/users/adityabisoi/followers')
+    .then((data) => printData(data.data))
+
+function printData(followerData) {
+    fs.readFile(__dirname + '/template.ejs', (err, data) => {
+        if (err) throw err
+        const output = ejs.render(data.toString(), {
+            followers: followerData
+        })
+        fs.writeFileSync('README.md', output);
+    })
+}
