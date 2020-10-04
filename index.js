ede25de2561c25a268972a4ace5fc3bb8b94aaf4
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-10-04 18:44:34 +0530

    add time

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ axios.get('https://api.github.com/users/adityabisoi/followers')
 function printData(followerData) {
     fs.readFile(__dirname + '/template.ejs', (err, data) => {
         if (err) throw err
+        const time = new Date().toLocaleString()
         const writeData = ejs.render(data.toString(), {
-            followers: followerData
+            followers: followerData,
+            time: time
         })
         fs.writeFileSync('README.md', writeData);
     })
